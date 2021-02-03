b266f7c2475c188e29891810ed6d9bfb1c2d993b
unknown <samanthamikhail24@gmail.com>
2021-02-03 12:41:00 +0800

    keeper app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Footer() {
+  const date = new Date();
+  const currentYear = date.getFullYear()
+
+  return <div>
+    <footer><p>Copyright © {currentYear}</p></footer>
+  </div>
+}
+
+export default Footer;
